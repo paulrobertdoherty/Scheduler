aa5d5a5387ad7c67366c3899e0d917009645e07a
Sam K <skorman101@gmail.com>
2016-11-18 12:54:17 -0700

    Almost Finished Calculate Method

--- src/org/usfirst/frc/team1339/utils/MotionProfile.java
+++ src/org/usfirst/frc/team1339/utils/MotionProfile.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team1339.utils;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class MotionProfile {
 	
-	public double Kp, Ki, Kd, Ka, Kv, goal, cruiseVel, maxAcc, cruiseVelScaleFactor;
+	public double Kp, Ki, Kd, Ka, Kv, goal, cruiseVel,
+	maxAcc, cruiseVelScaleFactor;
+	private double lastTime;
 	public Segment initialSegment = new Segment(0, 0, 0);
 	public Segment currentSegment;
 	public Segment nextSegment;
@@ -17,6 +21,20 @@ public class MotionProfile {
 		this.Kd = Kd;
 		this.Ka = Ka;
 		this.Kv = Kv;
+	}
+	
+	private enum MotionState{
+		ACCELERATING, CRUISING, DECELERATING, END
+	}
+	
+	private MotionState state = MotionState.END;
+	
+	private void setState(MotionState newState){
+		state = newState;
+	}
+	
+	private MotionState getState(){
+		return state;
 	}
 
 	public static class Segment{
@@ -35,6 +53,8 @@ public class MotionProfile {
 		this.maxAcc = Constants.maxAcceleration;
 		this.currentSegment = initialSegment;
 		this.cruiseVelScaleFactor = Constants.motionProfileSlowScaleFactor;
+		setState(MotionState.ACCELERATING);
+		lastTime = Timer.getFPGATimestamp();
 	}
 	
 	public void configureNewProfile(double Kp, double Ki, double Kd, double Ka,
@@ -49,6 +69,8 @@ public class MotionProfile {
 		this.maxAcc = Constants.maxAcceleration;
 		this.currentSegment = initialSegment;
 		this.cruiseVelScaleFactor = Constants.motionProfileSlowScaleFactor;
+		setState(MotionState.ACCELERATING);
+		lastTime = Timer.getFPGATimestamp();
 	}
 	
 	public void setProfileConstants(double Kp, double Ki, double Kd, double Ka,
@@ -65,4 +87,63 @@ public class MotionProfile {
 		double maxVelOverHalfDistance = Math.sqrt(2 * halfDist * maxAcc);
 		return Math.min(maxVelOverHalfDistance * cruiseVelScaleFactor, Constants.maxCruiseSpeed);
 	}
+	
+	public void initializeTime(){
+		lastTime = Timer.getFPGATimestamp();
+	}
+	
+	public void calculate(){
+		double dt;
+		double currentTime = Timer.getFPGATimestamp();
+		dt = lastTime - currentTime;
+		lastTime = currentTime;
+		double currentVel = currentSegment.vel;
+		double distanceToGo = goal - currentSegment.pos;
+		
+		double t_to_cruise = (cruiseVel - currentVel) / maxAcc; //time to accelerate to cruise speed
+		double x_to_cruise = currentVel * t_to_cruise + .5 * maxAcc * t_to_cruise * t_to_cruise; //distance to get to cruise speed
+		
+		double t_to_zero = Math.abs(cruiseVel / maxAcc); //time to get to zero speed from cruise speed
+		double x_to_zero =currentVel * t_to_zero + .5 * maxAcc * t_to_zero * t_to_zero; //distance to get to zero speed
+		
+		double cruiseX  = Math.max(0, distanceToGo - x_to_cruise - x_to_zero);
+		double cruiseT = Math.abs(cruiseX / cruiseVel);
+		
+		if (getState() == MotionState.ACCELERATING){
+			if (t_to_cruise < dt){
+				setState(MotionState.CRUISING);
+			}
+		}
+		
+		if(getState() == MotionState.CRUISING){
+			if(t_to_cruise + cruiseT < dt){
+				setState(MotionState.DECELERATING);
+			}
+		}
+		
+		if(getState() == MotionState.DECELERATING){
+			if(t_to_cruise + cruiseT + t_to_zero < dt){
+				setState(MotionState.END);
+			}
+		}
+		
+		if(getState() == MotionState.ACCELERATING){
+			nextSegment.pos = currentVel * dt + .5 * maxAcc * dt * dt;
+			nextSegment.vel = currentVel + dt * maxAcc;
+			nextSegment.acc = maxAcc;
+		}
+		if(getState() == MotionState.CRUISING){
+			nextSegment.pos = x_to_cruise + cruiseVel * (dt - t_to_cruise);;
+			nextSegment.vel = cruiseVel;
+			nextSegment.acc = 0;
+		}
+		if(getState() == MotionState.DECELERATING){
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
