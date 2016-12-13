4b4863b2632ef15491eca8f2083043cf81d57845
Sam K <skorman101@gmail.com>
2016-12-13 12:54:29 -0700

    Commented Looper class

--- src/org/usfirst/frc/team1339/utils/Looper.java
+++ src/org/usfirst/frc/team1339/utils/Looper.java
@@ -12,7 +12,9 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;	
 
 /**
- * The Looper class calls and creates subsystems and runs them at certain rates.
+ * The Looper class loops through a list of commands that are supposed to be 
+ * scheduled and runs them at certain rates, depending on what runTime is set 
+ * within the command.
  * @author Sam Schwartz
  * @author Nate Howard
  * @author Sam Korman
@@ -21,40 +23,56 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 
 public class Looper {
-	/** Creating an array list of subsystems.*/
 	
+	//Global Instance of Looper Class
 	private static Looper instance;
 	
+	/** creating an arrayList of commands that are supposed to be run */
 	private static ArrayList<CommandBase> commands = new ArrayList<CommandBase>();
 	
 	public Looper(){
 		
 	}
 	
+	/** returns global instance */
 	public static Looper getInstance(){
 	    return instance == null ? instance = new Looper() : instance;
 	}
 	
+	/** 
+	 * This method adds the command @param instance to the list of running commands.
+	 * It checks if the command is running already, and if not, then it removes
+	 * the current command from the subsystem, ends it, and then is added to the list
+	 * of commands.
+	 * @param instance
+	 */
 	public void newCommand(CommandBase instance){
 		boolean isCommandAlready = false;
+		
+		//checking if command already exists
 		for(CommandBase command : commands){
 			if(command.getName().equals(instance.getName())) isCommandAlready = true;
 		}
+		
+		//only runs if it doesn't exist already
 		if(!isCommandAlready){
 			ArrayList<SubsystemBase> requirements = instance.getRequirements();
-			for(SubsystemBase subsystem : requirements){
+			for(SubsystemBase subsystem : requirements){  //loops through list of requirements
 				if (subsystem != null){
 					if(subsystem.getCurrentCommand() != null){
-						commands.remove(subsystem.getCurrentCommand());
-						subsystem.getCurrentCommand().cancel();
+						commands.remove(subsystem.getCurrentCommand()); //removes current command if it exists
+						subsystem.getCurrentCommand().cancel(); //cancels current command if it exists
 					}
-					subsystem.setCurrentCommand(instance);
+					subsystem.setCurrentCommand(instance); //sets instance to current command
 				}
 			}
-			commands.add(instance);
+			commands.add(instance); //adds instance to commands list
 		}
 	}
 	
+	/**
+	 * this method if for initializing the default commands at the start of teleOp
+	 */
 	public void setInitDefaults(){
 		commands.clear();
 		for (SubsystemBase subsystem : SubsystemBase.getDefaults()){
@@ -65,7 +83,7 @@ public class Looper {
 		}
 	}
 	/**
-	 * This method updates subsystems.
+	 * This method is run continuously to run the commands.
 	 * <p> $ <p> 
 	 * @see SubsystemBase
 	 */
