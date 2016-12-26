1c6410d33bc7a201842f5bc0bc5c36992eea4aa6
earobotics <angelbots1339@gmail.com>
2016-12-26 14:48:28 -0700

    SubsystemBase is fully commented.

--- src/org/usfirst/frc/team1339/base/SubsystemBase.java
+++ src/org/usfirst/frc/team1339/base/SubsystemBase.java
@@ -1,13 +1,10 @@
 package org.usfirst.frc.team1339.base;
 
-import java.util.ArrayList;
-
 import org.usfirst.frc.team1339.utils.Looper;
 
 /**
- * The subsystem class is the very core of the entire subsystem framework.
- * Every subsystem is called through {@link run()} and run in Looper.
- * <br>$<br>
+ * This is the base class of all subsystems, and all subsystems should extend this class.
+ * 
  * @author Sam Schwartz
  * @author Nate Howard
  * @author Sam Korman
@@ -15,41 +12,66 @@ import org.usfirst.frc.team1339.utils.Looper;
 	
 public abstract class SubsystemBase {
 	
-	private CommandBase currentCommand, defaultCommand;
+	/**
+	 * A variable that contains the instance of the command that we are currently running.
+	 */
+	private CommandBase currentCommand;
 	
-	private static ArrayList<SubsystemBase> defaults = new ArrayList<SubsystemBase>();
+	/**
+	 * A variable that contains the default command of this subsystem.
+	 */
+	private CommandBase defaultCommand;
 	
+	/**
+	 * The method in which you should call the method {@link #setDefaultCommand(CommandBase)}.
+	 */
 	public abstract void initDefaultCommand();
 	
+	/**
+	 * The constructor for {@link SubsystemBase}. This automatically registers this subsystem to
+	 * {@link Looper} when any subsystem is constructed.
+	 */
 	protected SubsystemBase(){
 		Looper.getInstance().register(this);
 	}
 	
+	/**
+	 * This method is used in the subsystem to set the default command of that subsystem.
+	 * 
+	 * @param command The command to be the default command
+	 */
 	protected void setDefaultCommand(CommandBase command){
 		defaultCommand = command;
 		currentCommand = defaultCommand;
-		defaults.add(this);
 	}
 	
+	/**
+	 * This method is used to set the current command of the subsystem. Use this
+	 * when changing commands.
+	 * 
+	 * @param command The new command that requires this subsystem.
+	 */
 	public void setCurrentCommand(CommandBase command){
 		currentCommand = command;
 	}
 	
-	public static ArrayList<SubsystemBase> getDefaults(){
-		return defaults;
-	}
-	
+	/**
+	 * This method returns the current command that is requiring this subsystem.
+	 * 
+	 * @return The current command
+	 */
 	public CommandBase getCurrentCommand(){
 		return currentCommand;
 	}
 	
+	/**
+	 * This method returns the default command, which is set by {@link #setDefaultCommand(CommandBase)}.
+	 * 
+	 * @return The default command
+	 */
 	public CommandBase getDefaultCommand(){
 		if(defaultCommand == null)
 			initDefaultCommand();
 		return defaultCommand;
-	}
-	
-	public void endScheduledCommand(){
-		currentCommand = getDefaultCommand();
 	}
 }
